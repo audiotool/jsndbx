fa729f732a2592be4ebfd6f5720832a6a5187b45
andremichelle <andre.michelle@gmail.com>
2021-08-17 13:41:10 +0200

    import soundfont

--- sampler.js
+++ sampler.js
@@ -1,20 +1,30 @@
+const importSourcesSelect = document.querySelector("#input-sources");
+const instrumentOptionMap = new Map();
 const listInstruments = (fileName, instruments) => {
-    const select = document.querySelector("#input-sources");
     const list = document.createElement("optgroup");
-    list.label = `SF: ${fileName}`;
+    list.label = `💾 ${fileName}`;
     for (let i = 0; i < instruments.length; i++) {
         const instrument = instruments[i];
         const numBytes = instrument.zones.reduce((n, zone) => zone.sample.data.byteLength + n, 0);
         const name = instrument.header.name;
         console.log(`${i}: ${name} > ${numBytes >> 10}kb`);
-
         const option = document.createElement("option");
         option.textContent = `${name} (${numBytes >> 10}kb)`;
         list.appendChild(option);
+        instrumentOptionMap.set(option, instrument);
     }
-    select.appendChild(list);
+    importSourcesSelect.appendChild(list);
 };
-
+document.querySelector("#import-source").onclick = () => {
+    const option = importSourcesSelect.options[importSourcesSelect.selectedIndex];
+    if (-1 === importSourcesSelect.selectedIndex) return;
+    if (undefined === option) return;
+    const instrument = instrumentOptionMap.get(option);
+    // TODO Convert and display instrument
+    console.log(instrument);
+    document.querySelector("#instrument-name").textContent = instrument.header?.name;
+    importSourcesSelect.selectedIndex = -1;
+};
 document.querySelector("#input-soundfont-file").oninput = event => {
     const target = event?.target;
     if (target === undefined) return;
@@ -27,12 +37,12 @@ document.querySelector("#input-soundfont-file").oninput = event => {
         if (null === sf) {
             alert(`${file.name} could not be imported.`);
         } else {
-            alert(`${file.name} successfully imported.`);
             listInstruments(file.name, sf.instruments);
         }
     };
     fileReader.onload = () => {
         try {
+            // noinspection JSCheckFunctionSignatures
             complete(SoundFont2.SoundFont2.from(new Uint8Array(fileReader.result)));
         } catch (e) {
             console.warn(e);
